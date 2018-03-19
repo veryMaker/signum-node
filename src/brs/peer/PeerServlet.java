e963077a2866e74c1f69e65cb538b316330881c7
rico666 <bots@cryptoguru.org>
2018-03-19 09:14:18 +0100

    backport of GzipFilter -> GzipHandler migration for Jetty 9.4 compatibility

--- src/brs/peer/PeerServlet.java
+++ src/brs/peer/PeerServlet.java
@@ -37,8 +37,10 @@ public final class PeerServlet extends HttpServlet {
 
   private final Map<String,PeerRequestHandler> peerRequestHandlers;
 
-  public PeerServlet(TimeService timeService, AccountService accountService, Blockchain blockchain, TransactionProcessor transactionProcessor,
-      BlockchainProcessor blockchainProcessor) {
+  public PeerServlet(TimeService timeService, AccountService accountService,
+                     Blockchain blockchain,
+                     TransactionProcessor transactionProcessor,
+                     BlockchainProcessor blockchainProcessor) {
     final Map<String,PeerRequestHandler> map = new HashMap<>();
     map.put("addPeers", AddPeers.instance);
     map.put("getCumulativeDifficulty", new GetCumulativeDifficulty(blockchain));
@@ -137,18 +139,12 @@ public final class PeerServlet extends HttpServlet {
     resp.setContentType("text/plain; charset=UTF-8");
     try {
       long byteCount;
-      if (isGzipEnabled) {
-        try (Writer writer = new OutputStreamWriter(resp.getOutputStream(), "UTF-8")) {
-          response.writeJSONString(writer);
-        }
-        byteCount = ((Response) ((HttpServletResponseWrapper) resp).getResponse()).getContentCount();
-      } else {
-        CountingOutputStream cos = new CountingOutputStream(resp.getOutputStream());
-        try (Writer writer = new OutputStreamWriter(cos, "UTF-8")) {
-          response.writeJSONString(writer);
-        }
-        byteCount = cos.getCount();
+
+      CountingOutputStream cos = new CountingOutputStream(resp.getOutputStream());
+      try (Writer writer = new OutputStreamWriter(cos, "UTF-8")) {
+        response.writeJSONString(writer);
       }
+      byteCount = cos.getCount();
       if (peer != null) {
         peer.updateUploadedVolume(byteCount);
       }
