8f71b79963a66b8b3faa205f35525158b6c22c2a
Brabantian <brabantianburst@gmail.com>
2018-11-28 22:19:10 +0100

    Let's not punish the CPU

--- src/brs/BlockchainProcessorImpl.java
+++ src/brs/BlockchainProcessorImpl.java
@@ -210,6 +210,11 @@ public final class BlockchainProcessorImpl implements BlockchainProcessor {
     int queueThreshold = oclVerify ? oclUnverifiedQueue : 0;
 
     while (!Thread.interrupted() && ThreadPool.running.get() ) {
+      try {
+        Thread.sleep(10);
+      } catch (InterruptedException ex) {
+        Thread.currentThread().interrupt();
+      }
       int unVerified = downloadCache.getUnverifiedSize();
       if (unVerified > queueThreshold) { //Is there anything to verify
         if (unVerified >= oclUnverifiedQueue && oclVerify) { //should we use Ocl?
