81fd71d6d7f3a1a60b9a3d7e3ddef7df010e6e38
Harry Phillips <harryjamesphillips@gmail.com>
2019-01-22 23:44:23 +0000

    v2.3.0-alpha1

--- src/brs/Burst.java
+++ src/brs/Burst.java
@@ -44,7 +44,7 @@ import static brs.schema.Tables.UNCONFIRMED_TRANSACTION;
 
 public final class Burst {
 
-  public static final Version VERSION = Version.parse("v2.3.0-dev");
+  public static final Version VERSION = Version.parse("v2.3.0-alpha1");
   public static final String APPLICATION = "BRS";
 
   private static final String DEFAULT_PROPERTIES_NAME = "brs-default.properties";
