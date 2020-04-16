3775f5146c486991e4d51af00f9f30071f4c72aa
jjos2372 <jjos2372@gmail.com>
2020-04-16 18:40:02 -0300

    unused import

--- src/brs/GeneratorImpl.java
+++ src/brs/GeneratorImpl.java
@@ -14,7 +14,6 @@ import burst.kit.crypto.BurstCrypto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.Collection;
 import java.util.Collections;
