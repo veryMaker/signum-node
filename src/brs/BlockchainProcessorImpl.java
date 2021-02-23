89b863d669d72a434d5837236323b93fcf28bd88
jjos2372 <jjos2372@gmail.com>
2021-02-23 12:42:11 -0300

    Report peer in case of a timestamp problem.

--- src/brs/BlockchainProcessorImpl.java
+++ src/brs/BlockchainProcessorImpl.java
@@ -887,7 +887,7 @@ public final class BlockchainProcessorImpl implements BlockchainProcessor {
             || block.getTimestamp() <= previousLastBlock.getTimestamp()) {
           throw new BlockOutOfOrderException("Invalid timestamp: " + block.getTimestamp()
               + " current time is " + curTime
-              + ", previous block timestamp is " + previousLastBlock.getTimestamp());
+              + ", previous block timestamp is " + previousLastBlock.getTimestamp() + ", peer is " + block.getPeer().getAnnouncedAddress());
         }
         if (block.getId() == 0L || blockDb.hasBlock(block.getId())) {
           throw new BlockNotAcceptedException("Duplicate block or invalid id for block " + block.getHeight());
