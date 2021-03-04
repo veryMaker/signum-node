42c80bf11b7b2b07ee55e835ebc034555e7705e2
jjos2372 <jjos2372@gmail.com>
2021-03-04 15:02:48 -0300

    New release.

--- src/brs/Burst.java
+++ src/brs/Burst.java
@@ -46,7 +46,7 @@ import java.util.regex.Pattern;
 
 public final class Burst {
 
-  public static final Version VERSION = Version.parse("v3.0.0-alpha10");
+  public static final Version VERSION = Version.parse("v3.0.0-alpha11");
 
   public static final String APPLICATION = "BRS";
 
