741fdf9d77cc6865fee3a9d79acc22ce9c139f0a
jjos2372 <jjos2372@gmail.com>
2021-03-28 10:58:59 -0300

    Fix block counting for classic wallet.

--- src/brs/db/sql/SqlBlockchainStore.java
+++ src/brs/db/sql/SqlBlockchainStore.java
@@ -47,15 +47,17 @@ public class SqlBlockchainStore implements BlockchainStore {
   @Override
   public Collection<Block> getBlocks(Account account, int timestamp, int from, int to) {
     return Db.useDSLContext(ctx -> {
-      int blockchainHeight = Burst.getBlockchain().getHeight();
       
       SelectConditionStep<BlockRecord> query = ctx.selectFrom(BLOCK).where(BLOCK.GENERATOR_ID.eq(account.getId()));
       if (timestamp > 0) {
         query.and(BLOCK.TIMESTAMP.ge(timestamp));
       }
-      if(from > 0 || to > 0) {
-    	query.and(BLOCK.HEIGHT.between(to > 0 ? blockchainHeight - to : 0).and(blockchainHeight - Math.max(from, 0)));
-      }
+      // TODO: this filter is not working.
+      // Additionally, if we filter the blocks foget counting becomes wrong. 
+//      if(from > 0 || to > 0) {
+//        int blockchainHeight = Burst.getBlockchain().getHeight();
+//        query.and(BLOCK.HEIGHT.between(to > 0 ? blockchainHeight - to : 0).and(blockchainHeight - Math.max(from, 0)));
+//      }
       return getBlocks(query.orderBy(BLOCK.HEIGHT.desc()).fetch());
     });
   }
