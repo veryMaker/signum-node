476742e1adbe6fa9557cfd2238f277e76c8a1a45
jjos2372 <jjos2372@gmail.com>
2021-04-01 18:27:39 -0300

    GetAccount API should return also the assets for the classic wallet.

--- src/brs/http/GetAccount.java
+++ src/brs/http/GetAccount.java
@@ -7,10 +7,12 @@ import brs.Burst;
 import brs.BurstException;
 import brs.Constants;
 import brs.Generator;
+import brs.services.AccountService;
 import brs.services.ParameterService;
 import brs.util.Convert;
 import burst.kit.crypto.BurstCrypto;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
@@ -26,12 +28,14 @@ public final class GetAccount extends APIServlet.JsonRequestHandler {
 
   private final ParameterService parameterService;
   private final Blockchain blockchain;
+  private final AccountService accountService;
   private final Generator generator;
 
-  GetAccount(ParameterService parameterService, Blockchain blockchain, Generator generator) {
+  GetAccount(ParameterService parameterService, AccountService accountService, Blockchain blockchain, Generator generator) {
     super(new APITag[] {APITag.ACCOUNTS}, ACCOUNT_PARAMETER, HEIGHT_PARAMETER, GET_COMMITTED_AMOUNT_PARAMETER, ESTIMATE_COMMITMENT_PARAMETER);
     this.parameterService = parameterService;
     this.blockchain = blockchain;
+    this.accountService = accountService;
     this.generator = generator;
   }
 
@@ -70,6 +74,32 @@ public final class GetAccount extends APIServlet.JsonRequestHandler {
     if (account.getDescription() != null) {
       response.addProperty(DESCRIPTION_RESPONSE, account.getDescription());
     }
+
+    if(height == blockchain.getHeight()) {
+      // Only if the height is the latest as we don't handle past asset balances.
+      // Returning assets here is needed by the classic wallet, so we keep it.
+      JsonArray assetBalances = new JsonArray();
+      JsonArray unconfirmedAssetBalances = new JsonArray();
+
+      for (Account.AccountAsset accountAsset : accountService.getAssets(account.getId(), 0, -1)) {
+        JsonObject assetBalance = new JsonObject();
+        assetBalance.addProperty(ASSET_RESPONSE, Convert.toUnsignedLong(accountAsset.getAssetId()));
+        assetBalance.addProperty(BALANCE_QNT_RESPONSE, String.valueOf(accountAsset.getQuantityQNT()));
+        assetBalances.add(assetBalance);
+        JsonObject unconfirmedAssetBalance = new JsonObject();
+        unconfirmedAssetBalance.addProperty(ASSET_RESPONSE, Convert.toUnsignedLong(accountAsset.getAssetId()));
+        unconfirmedAssetBalance.addProperty(UNCONFIRMED_BALANCE_QNT_RESPONSE, String.valueOf(accountAsset.getUnconfirmedQuantityQNT()));
+        unconfirmedAssetBalances.add(unconfirmedAssetBalance);
+      }
+
+      if (assetBalances.size() > 0) {
+        response.add(ASSET_BALANCES_RESPONSE, assetBalances);
+      }
+      if (unconfirmedAssetBalances.size() > 0) {
+        response.add(UNCONFIRMED_ASSET_BALANCES_RESPONSE, unconfirmedAssetBalances);
+      }
+    }
+
     return response;
   }
 
