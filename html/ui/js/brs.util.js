b9ac3504562ef7cf14efb3ee15e0c418aecce02f
jjos2372 <jjos2372@gmail.com>
2021-04-02 14:24:42 -0300

    Format numbers with the English locale.

--- html/ui/js/brs.util.js
+++ html/ui/js/brs.util.js
@@ -236,7 +236,7 @@ var BRS = (function(BRS, $, undefined) {
         } while (volume > 0);
         for (i = 0; i < digits.length; i++) {
             if (i > 0 && i % 3 === 0) {
-                formattedVolume = "'" + formattedVolume;
+                formattedVolume = "," + formattedVolume;
             }
             formattedVolume = digits[i] + formattedVolume;
         }
@@ -512,7 +512,7 @@ var BRS = (function(BRS, $, undefined) {
 
         for (var i = 0; i < digits.length; i++) {
             if (i > 0 && i % 3 === 0) {
-                formattedAmount = "'" + formattedAmount;
+                formattedAmount = "," + formattedAmount;
             }
             formattedAmount = digits[i] + formattedAmount;
         }
