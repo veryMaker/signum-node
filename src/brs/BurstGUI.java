24dd7cdb7fd05ed2b770b5ce3e4ef217afda190a
jjos2372 <jjos2372@gmail.com>
2021-04-11 10:07:37 -0300

    Back to a dark theme.

--- src/brs/BurstGUI.java
+++ src/brs/BurstGUI.java
@@ -92,6 +92,20 @@ public class BurstGUI extends JFrame {
         }
         if(lafc!=null) {
           try {
+            UIManager.put( "control", new Color( 128, 128, 128) );
+            UIManager.put( "info", new Color(128,128,128) );
+            UIManager.put( "nimbusBase", new Color( 18, 30, 49) );
+            UIManager.put( "nimbusAlertYellow", new Color( 248, 187, 0) );
+            UIManager.put( "nimbusDisabledText", new Color( 128, 128, 128) );
+            UIManager.put( "nimbusFocus", new Color(115,164,209) );
+            UIManager.put( "nimbusGreen", new Color(176,179,50) );
+            UIManager.put( "nimbusInfoBlue", new Color( 66, 139, 221) );
+            UIManager.put( "nimbusLightBackground", new Color( 18, 30, 49) );
+            UIManager.put( "nimbusOrange", new Color(191,98,4) );
+            UIManager.put( "nimbusRed", new Color(169,46,34) );
+            UIManager.put( "nimbusSelectedText", new Color( 255, 255, 255) );
+            UIManager.put( "nimbusSelectionBackground", new Color( 104, 93, 156) );
+            UIManager.put( "text", new Color( 230, 230, 230) );
             LookAndFeel laf = (LookAndFeel) lafc.getConstructor().newInstance();
             UIManager.setLookAndFeel(laf);                          
           } catch (Exception e) {
