4b872f5bd8ee7900162d61c4fadaa5e0dddb7e74
jjos2372 <jjos2372@gmail.com>
2021-06-25 14:55:46 -0300

    Return integer as string.

--- src/brs/http/GetMiningInfo.java
+++ src/brs/http/GetMiningInfo.java
@@ -39,7 +39,7 @@ final class GetMiningInfo extends APIServlet.JsonRequestHandler {
     response.addProperty(ResultFields.BASE_TARGET_RESPONSE, Long.toString(lastBlock.getCapacityBaseTarget()));
     response.addProperty(ResultFields.AVERAGE_COMMITMENT_NQT_RESPONSE, Long.toString(lastBlock.getAverageCommitment()));
     response.addProperty(ResultFields.LAST_BLOCK_REWARD_RESPONSE, Long.toString(blockService.getBlockReward(lastBlock)/Constants.ONE_BURST));
-    response.addProperty(ResultFields.TIMESTAMP_RESPONSE, lastBlock.getTimestamp());
+    response.addProperty(ResultFields.TIMESTAMP_RESPONSE, Long.toString((long)lastBlock.getTimestamp()));
 		
     return response;
   }
