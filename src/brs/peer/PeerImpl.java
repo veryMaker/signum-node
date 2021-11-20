4fff45b33c7f9ecccaa3ab35414feec236d07ef7
jjos2372 <jjos2372@gmail.com>
2021-11-20 14:18:08 -0300

    Accept announced peer with port.

--- src/brs/peer/PeerImpl.java
+++ src/brs/peer/PeerImpl.java
@@ -426,8 +426,11 @@ final class PeerImpl implements Peer {
       platform.set(JSON.getAsString(response.get("platform")));
       shareAddress.set(Boolean.TRUE.equals(JSON.getAsBoolean(response.get("shareAddress"))));
       String newAnnouncedAddress = Convert.emptyToNull(JSON.getAsString(response.get("announcedAddress")));
+      int port = this.port.get();
+      if(port < 0)
+        port = Burst.getPropertyService().getInt(Props.P2P_PORT);
       if (newAnnouncedAddress != null && ! newAnnouncedAddress.equals(announcedAddress.get())
-          && !newAnnouncedAddress.equals(announcedAddress.get() + ":" + port.get())) {
+          && !newAnnouncedAddress.equals(announcedAddress.get() + ":" + port)) {
         // force verification of changed announced address
         setState(Peer.State.NON_CONNECTED);
         setAnnouncedAddress(newAnnouncedAddress);
