6d407a27f31c02087ed968e8dff9b9bbcdb66ced
jjos2372 <jjos2372@gmail.com>
2021-12-03 22:10:46 -0300

    Check for the quantity only for non mintable assets.

--- src/brs/TransactionType.java
+++ src/brs/TransactionType.java
@@ -1417,7 +1417,7 @@ public abstract class TransactionType {
           throw new BurstException.NotValidException("Invalid asset order placement: " + JSON.toJsonString(attachment.getJsonObject()));
         }
         Asset asset = assetExchange.getAsset(attachment.getAssetId());
-        if (attachment.getQuantityQNT() <= 0 || (asset != null && attachment.getQuantityQNT() > asset.getQuantityQNT())) {
+        if (attachment.getQuantityQNT() <= 0) {
           throw new BurstException.NotValidException("Invalid asset order placement asset or quantity: " + JSON.toJsonString(attachment.getJsonObject()));
         }
         if (asset == null) {
