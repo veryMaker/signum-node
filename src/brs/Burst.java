73fef78c6638d1a29dd595bee179b7eecd818640
jjos2372 <jjos2372@gmail.com>
2021-12-29 10:35:59 -0300

    Version number.

--- src/brs/Burst.java
+++ src/brs/Burst.java
@@ -46,7 +46,7 @@ import java.util.regex.Pattern;
 
 public final class Burst {
 
-  public static final Version VERSION = Version.parse("v3.3.0-alpha9");
+  public static final Version VERSION = Version.parse("v3.3.0-alpha10");
   public static final String APPLICATION = "BRS";
 
   public static final String CONF_FOLDER = "./conf";
