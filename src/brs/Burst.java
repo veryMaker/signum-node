c311c712dff6973047981785bd7d0cc7bc445515
jjos2372 <jjos2372@gmail.com>
2022-05-14 09:52:34 -0300

    Version.

--- src/brs/Burst.java
+++ src/brs/Burst.java
@@ -47,7 +47,7 @@ import java.util.regex.Pattern;
 
 public final class Burst {
 
-  public static final Version VERSION = Version.parse("v3.4.0-beta5");
+  public static final Version VERSION = Version.parse("v3.4.0-beta8");
   public static final String APPLICATION = "BRS";
 
   public static final String CONF_FOLDER = "./conf";
