3d1d6b3494c1c4718eea40f0c92ea87e677daf8a
damccull <401104+damccull@users.noreply.github.com>
2024-02-08 01:23:38 -1000

    Add CaselessProperties.java

---
+++ src/brs/props/CaselessProperties.java
@@ -0,0 +1,34 @@
+package brs.props;
+
+import java.util.Properties;
+
+/**
+ * Overrides {@link java.util.Properties} to make all the keys lowercase,
+ * thus making the config file keys case-insensitve.
+ * Courtesy Jim Yingst https://coderanch.com/t/277128/java/Properties-ignoring-case
+ */
+public class CaselessProperties extends Properties {
+
+    public CaselessProperties() {
+        super();
+    }
+
+    public CaselessProperties(CaselessProperties properties) {
+        super(properties);
+    }
+
+    public Object put(Object key, Object value) {
+        String lowercase = ((String) key).toLowerCase();
+        return super.put(lowercase, value);
+    }
+
+    public String getProperty(String key) {
+        String lowercase = key.toLowerCase();
+        return super.getProperty(lowercase);
+    }
+
+    public String getProperty(String key, String defaultValue) {
+        String lowercase = key.toLowerCase();
+        return super.getProperty(lowercase, defaultValue);
+    }
+}
